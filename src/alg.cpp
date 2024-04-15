16d47abefa90a09ce9b0edbc017004279db0107d
KozzhevnikovAA <116709158+KozzhevnikovAA@users.noreply.github.com>
2024-04-15 11:10:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,28 @@
 // Copyright 2021 NNTU-CS
-
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int st = 0;
+int end = size - 1;
+int count = 0;
+while (st <= end) {
+int half = (st + end) / 2;
+if (arr[half] == value) {
+count++;
+int i = half - 1;
+while (i >= st && arr[i] == value) {
+count++;
+i--;
+}
+int k = half + 1;
+while (k <= end && arr[k] == value) {
+count++;
+k++;
+}
+return count;
+} else if (arr[half] < value) {
+st = half + 1;
+} else {
+end = half - 1;
+}
+}
+return 0;
 }
